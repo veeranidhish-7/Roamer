cfe8005489a2816c8663cf5b9386bc4466d5ece4
veeranidhish <g.veeranidhish014@gmail.com>
2026-01-25 12:21:51 +0530

    Phase 2 (Part d) Completed {Just implemented authentication and learnt PASSPORT package} (Login)

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 async function main() {
@@ -50,6 +54,13 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 //Middle ware for showing the flash message
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
@@ -57,8 +68,19 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+app.get("/demouser", async (req, res) => {
+  let fakeUser = new User({
+    email: "student@gmail.com",
+    username: "delta-student",
+  });
+
+  let registeredUser = await User.register(fakeUser, "helloworld");
+  res.send(registeredUser);
+});
+
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 // Change "*" to /(.*)/
 app.all(/(.*)/, (req, res, next) => {
---
+++ routes/user.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ email, username });
+      const registerdUser = await User.register(newUser, password);
+      console.log(registerdUser);
+      req.flash("success", "Welcome to WanderLust");
+      res.redirect("/listings");
+    } catch (er) {
+      req.flash("error", er.message);
+      res.redirect("/signup");
+    }
+  }),
+);
+
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    let { username } = req.body;
+    req.flash("success", `Welcome Back to WanderLust, ${username}`);
+    res.redirect("/listings");
+  },
+);
+
+module.exports = router;
